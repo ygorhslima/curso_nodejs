f8c7c5bfb431487d4d36be8c1603b0c208828cc3
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-10-18 15:53:08 -0300

    .

--- introducao_express/index.js
+++ introducao_express/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
-
+import path from 'path';
 // recebendo a função do express
 //qualquer coisa que você utilizar do express vem desta constante
-
 const app = express();
+const __dirname = path.resolve();
 
 app.get('/',(req,res)=>{
-    res.send("seja bem-vindo ao meu site");
-});
+    res.sendFile(__dirname + "/index.html")
+})
 
 app.get('/sobre',(req,res)=>{
     res.send("Minha pagina sobre");
@@ -20,7 +20,7 @@ app.get("/blog",(req,res)=>{
 app.get("/ola/:nome/:cargo/:cor",(req,res)=>{
     res.send(`
         <h1>ola ${req.params.nome}</h1> 
-        a sua profissão é ${req.params.cargo} <br>
+        <p>a sua profissão é ${req.params.cargo}</p><br>
         e sua cor favorita é ${req.params.cor} <br>
     `); // você só pode usar uma send em um get
 });
@@ -28,3 +28,10 @@ app.get("/ola/:nome/:cargo/:cor",(req,res)=>{
 app.listen(8081, ()=>{
     console.log("servidor rodando na url http://localhost:8081");
 }); // ela é a última linha do código da aplicação
+
+/**nodemon
+ * 
+ * importação muito importante no node, para automatizar o processo de iniciar e desligar o servidor
+ * npm install nodemon
+ * 
+*/
