38c59873dfb19b9273408f8258300a1d11e18932
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-10-18 10:46:37 -0300

    revisando módulo

---
+++ modulos/index.js
@@ -0,0 +1,10 @@
+import somaFunc from './somar.js'
+import subtrairFunc from './subtrair.js'
+import multiplicaFunc from './multiplicar.js'
+import divFunc from './dividir.js'
+
+
+console.log(somaFunc(10,10))
+console.log(subtrairFunc(10,5))
+console.log(multiplicaFunc(20,5))
+console.log(divFunc(10,2))
